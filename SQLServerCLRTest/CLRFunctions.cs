186a7f45bb00e84ed85b04e2ae5c7a6097d60bc2
Ahmed Tarek Hasan <eng_ahmed.tarek@hotmail.com>
2017-09-28 00:27:06 +0200

    Add files via upload

---
+++ SQLServerCLRTest/CLRFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SQLServerClrNS
+{
+    /*
+    You can find the SQL script you need to run on SQL DB in the file "SQLScript.txt" in 
+    this "SQLServerCLR" project
+    */
+
+    public class CLRFunctions
+    {
+        public static string HelloWorld(string name)
+        {
+            return "Hello " + name;
+        }
+
+        public static string ReplaceWithRegExp(string SourceStr, string RegExp, string ReplacementStr)
+        {
+            return Regex.Replace(SourceStr, RegExp, ReplacementStr);
+        }
+    }
+}
